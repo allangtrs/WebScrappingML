2de827d3aecd79d55b189869e223a120360c479b
Allan Corrêa <105672212+allangtrs@users.noreply.github.com>
2023-03-27 16:21:24 -0300

    Add files via upload
    first commit

---
+++ ml/ml/spiders/mlpromotion.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class MlpromotionSpider(scrapy.Spider):
+    name = 'mlpromotion'
+    start_urls = ['https://www.mercadolivre.com.br/ofertas#nav-header']
+
+    def parse(self, response, **kwargs):
+        cont = 0
+        for i in response.css('.promotion-item__description'):
+            price = i.css('.andes-money-amount--cents-superscript .andes-money-amount__fraction::text').get()
+            title = i.css('.promotion-item__title::text').get()
+            link = response.css('.items_container a::attr(href)')[cont].get()
+
+
+            yield {
+                'price': price,
+                'title': title,
+                'link': link
+            }
+            cont+=1
+
+        prox_pag = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "andes-pagination__button--next", " " ))]//*[contains(concat( " ", @class, " " ), concat(" ", "andes-pagination__link", " " ))]').attrib['href']
+        if prox_pag is not None:
+            yield response.follow(prox_pag, callback=self.parse)
+
+e
+
+
+
+
